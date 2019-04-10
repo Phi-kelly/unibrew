b2956a431fd07be19564b4802bc02a4dc07c1488
Phi <molel@icloud.com>
2019-04-10 21:30:46 +0200

    Modifies the style file for more clarifications adds menus functions for both main nav and footer nav

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <footer class="nav nav-navbar-bottom">
+<?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
     <small> &copy; 2018, Royal Unibrew A/S </small>
 </footer>
+<?php wp_footer(); ?>
 </body>
 
 </html>
--- functions.php
+++ functions.php
@@ -1,9 +1,18 @@
 <?php
 function css_script_scripts_and_styles()
 {
-    wp_enqueue_style('bootstrap-cdn-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
-    wp_enqueue_script('bootstrap-cdn-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js');
-    wp_enqueue_style('styles', get_template_directory_uri() . "/css/styles.css");
+    wp_enqueue_style('bootstrap-cdn-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', 'all');
+    wp_enqueue_script('bootstrap-cdn-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js', true);
+    wp_enqueue_style('styles', get_template_directory_uri() . "/css/styles.css", 'all');
 }
 
 add_action('wp_enqueue_scripts', 'css_script_scripts_and_styles');
+
+function awesome_theme_setup(){
+    add_theme_support('menus');
+    register_nav_menu('primary', 'Primary Header Navigation');
+    register_nav_menu('secondary', 'Footer Navigation');
+
+}
+
+add_action('init', 'awesome_theme_setup');
